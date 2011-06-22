b4261a66e280866ced2826751e486378e59c246d
Stefan Müller <co@zzeroo.com>
2011-06-22 21:44:18 +0200

    Initial commit
    vanilla rails 3 app with drnic's magic_model_gem installed.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Darkcoinpool::Application.config.secret_token = 'bcb130893feb5593bb6f175139dac623b3a3123f0bd5fe5e9291e13f32438292e130b2f89e30d68d20c02364c592297291af1f2527ad1c6110fcb6e39dde8860'
